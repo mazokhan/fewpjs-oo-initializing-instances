d2c19ff156ab3b0de5fec4cc7883f28805381c15
mazen <mak9986@hotmail.com>
2021-11-07 22:35:31 +1100

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class breakfast{
+    constructor(food,drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+class lunch {
+    constructor(salad,soup,drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+class Dinner {
+    constructor (salad,soup,entree,desert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._desert = desert
+    }
+}
